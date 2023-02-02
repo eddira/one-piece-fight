af543dca71bbad8facbd69eda7f69ef5486c6118
Mehdi Eddira <m.eddira75@outlook.fr>
2023-02-02 11:44:12 +0100

    second commit

--- script.js
+++ script.js
@@ -1,121 +1,122 @@
-const players = [
-  { name: "MONKEY-D.LUFFY", img: "1-MONKEY-D.LUFFY.png", strength: 9 },
-  { name: "BLACK-BEARD", img: "2-BLACK-BEARD.png", strength: 8 },
-  { name: "KAIDO", img: "3-KAIDO.png", strength: 7 },
-  { name: "BIG-MOM", img: "4-BIG-MOM.png", strength: 6 },
-  { name: "SHANKS", img: "5-SHANKS.jpg", strength: 5 },
-  { name: "SENGOKU", img: "6-SENGOKU.jpg", strength: 4 },
-  { name: "SAKAZUKI-AKAINU", img: "7-SAKAZUKI-AKAINU.jpg", strength: 3 },
-  { name: "MONKEY-D.DRAGON", img: "8-MONKEY-D.DRAGON.png", strength: 2 },
-  { name: "SILVERS-RAYLEIGH", img: "9-SILVERS-RAYLEIGH.jpg", strength: 1 },
-];
-
 const choices = document.querySelectorAll("#pictures > img");
 const yourPlayerDisplay = document.querySelector("#your-player-display");
-const figthButton = document.querySelector(".fight");
+const figthButton = document.querySelector("#start-fight");
 const computerDisplay = document.querySelector("#your-computer-display");
-let resultDisplay = document.querySelector(".result");
-let boxDialog = document.querySelector("#box-dialog");
-let btnReset = document.querySelector("#reset");
+const resultDisplay = document.querySelector("#result");
+const boxDialog = document.querySelector("#box-dialog");
+const btnReset = document.getElementById("reset");
+const yourCounter = document.querySelector(".your-counter");
+const computerCounter = document.querySelector(".computer-counter");
 
 let yourChoice = "";
 let computerChoice = "";
-let yourCounter = document.querySelector(".your-counter");
-let computerCounter = document.querySelector(".computer-counter");
 let yourScore = 0;
 let computerScore = 0;
 
 choices.forEach((choice) => {
   choice.addEventListener("click", (event) => {
     yourChoice = choice.alt;
-    // console.log(yourChoice);
     yourPlayerDisplay.src = choice.src;
   });
 });
-
-const getComputerChoice = () => {
-  const randomNumber = Math.floor(Math.random() * players.length);
-  const randomCharacter = choices[randomNumber];
-  computerChoice = randomCharacter.alt;
-  computerDisplay.src = randomCharacter.src;
-};
 
 const fight = () => {
   let result = "YOU LOOSE";
   if (
     yourChoice === "MONKEY-D.LUFFY" &&
-    (computerChoice === "BLACK-BEARD" || computerChoice === "KAIDO")
+    (computerChoice === "BLACK-BEARD" ||
+      computerChoice === "KAIDO" ||
+      computerChoice === "BIG-MOM")
   ) {
     result = "YOU WIN";
   }
   if (
     yourChoice === "BLACK-BEARD" &&
-    (computerChoice === "KAIDO" || computerChoice === "BIG-MOM")
+    (computerChoice === "KAIDO" ||
+      computerChoice === "BIG-MOM" ||
+      computerChoice === "SHANKS")
   ) {
     result = "YOU WIN";
   }
   if (
     yourChoice === "KAIDO" &&
-    (computerChoice === "BIG-MOM" || computerChoice === "SHANKS")
+    (computerChoice === "BIG-MOM" ||
+      computerChoice === "SHANKS" ||
+      computerChoice === "SENGOKU")
   ) {
     result = "YOU WIN";
   }
-
   if (
     yourChoice === "BIG-MOM" &&
-    (computerChoice === "SHANKS" || computerChoice === "SENGOKU")
+    (computerChoice === "SHANKS" ||
+      computerChoice === "SENGOKU" ||
+      computerChoice === "SAKAZUKI-AKAINU")
   ) {
     result = "YOU WIN";
   }
-
   if (
     yourChoice === "SHANKS" &&
-    (computerChoice === "SENGOKU" || computerChoice === "SAKAZUKI-AKAINU")
+    (computerChoice === "SENGOKU" ||
+      computerChoice === "SAKAZUKI-AKAINU" ||
+      computerChoice === "MONKEY-D.DRAGON")
   ) {
     return "YOU WIN";
   }
-
   if (
     yourChoice === "SENGOKU" &&
     (computerChoice === "SAKAZUKI-AKAINU" ||
-      computerChoice === "MONKEY-D.DRAGON")
-  ) {
-    result = "YOU WIN";
-  }
-
-  if (
-    yourChoice === "SAKAZUKI-AKAINU" &&
-    (computerChoice === "MONKEY-D.DRAGON" ||
+      computerChoice === "MONKEY-D.DRAGON" ||
       computerChoice === "SILVERS-RAYLEIGH")
   ) {
     result = "YOU WIN";
   }
-
+  if (
+    yourChoice === "SAKAZUKI-AKAINU" &&
+    (computerChoice === "MONKEY-D.DRAGON" ||
+      computerChoice === "SILVERS-RAYLEIGH" ||
+      computerChoice === "MONKEY-D.LUFFY")
+  ) {
+    result = "YOU WIN";
+  }
   if (
     yourChoice === "MONKEY-D.DRAGON" &&
     (computerChoice === "SILVERS-RAYLEIGH" ||
-      computerChoice === "MONKEY-D.LUFFY")
+      computerChoice === "MONKEY-D.LUFFY" ||
+      computerChoice === "BLACK-BEARD")
+  ) {
+    result = "YOU WIN";
+  }
+  if (
+    yourChoice === "SILVERS-RAYLEIGH" &&
+    (computerChoice === "MONKEY-D.LUFFY" ||
+      computerChoice === "BLACK-BEARD" ||
+      computerChoice === "KAIDO")
   ) {
     result = "YOU WIN";
   }
   if (yourChoice === computerChoice) {
     result = "IT'S A TIE";
   }
-
   return result;
 };
 
 const play = () => {
-  getComputerChoice();
   fight();
-  displayScore(fight());
-  // ajouter un timeout de 500ms avant de check if finished
+  getComputerChoice();
   displayResult(fight());
-  //timeoutGame()
+  hideResult();
+  displayScore(fight());
   checkIfFinished();
 };
 
 figthButton.addEventListener("click", play);
+
+function getComputerChoice() {
+  const randomNumber = Math.floor(Math.random() * 9);
+  const randomCharacter = choices[randomNumber];
+  computerChoice = randomCharacter.alt;
+  computerDisplay.src = randomCharacter.src;
+}
 
 function displayScore(result) {
   if (result === "YOU WIN") {
@@ -142,11 +143,11 @@ function displayResult(result) {
 
 function checkIfFinished() {
   if (yourScore === 5) {
+    boxDialog.showModal();
+    document.getElementById("winner").textContent = "YOU !";
     //   alert("YOU WIN THIS GAME")
     //   boxDialog.style.visibility = boxDialog.style.visibility == "visible" ? "hidden" : "visible";
     //   document.getElementById('winner').textContent = "YOU !"
-    boxDialog.showModal();
-    document.getElementById("winner").textContent = "YOU !";
   } else if (computerScore === 5) {
     //   alert("YOU LOOSE");
     //  boxDialog.style.visibility = boxDialog.style.visibility == "visible" ? "hidden" : "visible";
@@ -161,16 +162,19 @@ btnReset.addEventListener("click", (event) => {
   computerScore = 0;
   yourCounter.textContent = 0;
   computerCounter.textContent = 0;
-
+  yourPlayerDisplay.src = "";
+  computerDisplay.src = "";
   boxDialog.close();
 });
+
+function hideResult() {
+  setTimeout(() => {
+    resultDisplay.textContent = "?";
+  }, 500);
+}
 
 //function timeoutGame() {
 // setTimeout(() => {
 // checkIfFinished()
 //  }, 500);
-//}
-
-//function openDialog() {
-// boxDialog.style.visibility = boxDialog.style.visibility == "visible" ? "hidden" : "visible";
 //}
